3c4aa4846b6469d3228d2937789fa410f23be31b
kupets <12082003Dima>
2024-08-04 01:05:00 +0300

    feat: write greeting func

---
+++ app.js
@@ -0,0 +1,8 @@
+const readline = require('readline');
+function start() {
+    console.log('Hello, it is hangman game!');
+    console.log('For start new game make a choice: 1 - start new game, 2 - exit');
+}
+function getUserInput(){
+
+}
